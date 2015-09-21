85e50583d16e7360d9b204efb0aace65b89d6582
Kawshal Chhetri <kawshalchhetri@gmail.com>
2015-09-21 23:32:27 +0530

    Adding Files

---
+++ js/custom/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ChhetriK on 06-09-2015.
+ */
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+    $('.modal-trigger').leanModal();
+    $('#modal1').openModal();
+});
